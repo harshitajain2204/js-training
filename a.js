6c24e4a0a2b3b70a197db2f2cd9f42737407a55e
harshitajain2204 <harshitajain220499@gmail.com>
2021-03-29 20:32:45 -0700

    stringtypes

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
-console.log("hello")
+//var
+let x=10;
+const y=20;
+//string1 
+console.log('the sum is:-'+ (x+y));
+
+//string2
+console.log("the sub is:-"+ (x-y));
+
+//string3
+console.log(`the sum is:- ${x+y}`);
