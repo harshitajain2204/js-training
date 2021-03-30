f8e72f8e49e93863b7394fdf2c77af603c3379e1
harshitajain2204 <harshitajain220499@gmail.com>
2021-03-30 04:06:08 -0700

    global or local var

--- variable.js
+++ variable.js
@@ -1,10 +1,14 @@
 //global variable
 
 let sub= "script";
-//function
+//function defination
 function subname()
 {
     console.log("subject name:- java"+sub);
+    //local variable
+    let m=100; 
+    const n=500;
+    console.log(m*n);
 }
 // function calling
 subname();
